8faa8f77ea8e7c8b97705f8aabdba10325a5200b
Jovanka <samudrajovanka20@gmail.com>
2025-07-08 14:17:55 +0700

    fix: add catch for error get slug

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -7,22 +7,27 @@ import { getAccessLink } from '@/repositories/shortLink';
 
 const RedirectPage = async ({ params }: { params: Promise<{ slug: string }> }) => {
   const { slug } = await params;
-  const accessLinkResponse = await getAccessLink(slug);
 
-  if (accessLinkResponse.status === 404) {
+  try {
+    const accessLinkResponse = await getAccessLink(slug);
+    
+    if (accessLinkResponse.status === 404) {
+      notFound();
+    }
+  
+    const accessLink = await accessLinkResponse.json();
+    const { originalUrl } = accessLink.data.shortLink;
+  
+    if (originalUrl) {
+      redirect(originalUrl);
+    }
+  
+    return (
+      <Text>Welcome to redirect page</Text>
+    );
+  } catch (error) {
     notFound();
   }
-
-  const accessLink = await accessLinkResponse.json();
-  const { originalUrl } = accessLink.data.shortLink;
-
-  if (originalUrl) {
-    redirect(originalUrl);
-  }
-
-  return (
-    <Text>Welcome to redirect page</Text>
-  );
 };
 
 export default RedirectPage;
