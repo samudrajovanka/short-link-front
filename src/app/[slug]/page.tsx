a9f77b711652fe58ff78192ba5935ac9891ee8e5
Jovanka <samudrajovanka20@gmail.com>
2025-07-08 14:29:07 +0700

    fix: redirect page

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,8 +1,5 @@
-import React from 'react';
+import { notFound, redirect, RedirectType } from 'next/navigation';
 
-import { notFound, redirect } from 'next/navigation';
-
-import Text from '@/components/ui/Text';
 import { getAccessLink } from '@/repositories/shortLink';
 
 const RedirectPage = async ({ params }: { params: Promise<{ slug: string }> }) => {
@@ -10,23 +7,23 @@ const RedirectPage = async ({ params }: { params: Promise<{ slug: string }> }) =
 
   try {
     const accessLinkResponse = await getAccessLink(slug);
-    
-    if (accessLinkResponse.status === 404) {
-      notFound();
-    }
   
     const accessLink = await accessLinkResponse.json();
     const { originalUrl } = accessLink.data.shortLink;
   
     if (originalUrl) {
-      redirect(originalUrl);
+      redirect(originalUrl, RedirectType.replace);
     }
   
-    return (
-      <Text>Welcome to redirect page</Text>
-    );
-  } catch (error) {
     notFound();
+  } catch (error) {
+    console.log('error', error);
+
+    if ((error as Response).status === 404) {
+      notFound();
+    }
+    
+    throw error;
   }
 };
 
